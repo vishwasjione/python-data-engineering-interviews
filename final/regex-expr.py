725e3c2804dd51840252c43fb4b433748e8aa9df
vishwasjione@gmail.com <Github@2575>
2022-03-02 08:16:53 -0500

    new files

--- final/regex-expr.py
+++ final/regex-expr.py
@@ -1,2 +1,19 @@
 import re
 
+s = "vishwas"
+
+# find characters between value-value
+# print(re.findall('[a-i]',s))
+
+s = "dsk kd $12898.00 dollars"
+
+print(re.findall('[$]+\d',s))
+print(re.findall('[$]*[0-9]',s))
+
+# print(re.findall('\d',s))
+
+
+# print(re.findall('\A\$+\d+.+\d',s))
+# print(re.findall('\A\$+\d+.+\d',s))
+# s = "12.00 dollars"
+# print(re.findall('\A\$|\d+.+\d',s))
